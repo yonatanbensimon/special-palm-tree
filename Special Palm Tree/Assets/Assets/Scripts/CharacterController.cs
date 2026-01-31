d4f1b145b259e5fae5fc0b40ee58217906d0ee1f
Yonatan Bensimon <yonatan.bensimon@mail.mcgill.ca>
2026-01-31 13:02:53 -0500

    Added Spot Lights

--- Special Palm Tree/Assets/Assets/Scripts/CharacterController.cs
+++ Special Palm Tree/Assets/Assets/Scripts/CharacterController.cs
@@ -6,10 +6,19 @@ public class CharacterController : MonoBehaviour
     public float playerSpeed = 4f;
     
     private Vector2 moveInput;
+    private LightController nearbyLight;
 
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    public void OnInteract(InputValue value)
+    {
+        if (value.isPressed && nearbyLight != null)
+        {
+            nearbyLight.increaseLight();
+        }
     }
 
     // Update is called once per frame
@@ -17,5 +26,24 @@ public class CharacterController : MonoBehaviour
     {
         Vector3 movement = new Vector3(moveInput.x, moveInput.y, 0);
         transform.position += movement * playerSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<LightController>(out var light))
+        {
+            nearbyLight = light;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.TryGetComponent<LightController>(out var light))
+        {
+            if (nearbyLight == light)
+            {
+                nearbyLight = null;
+            }
+        }
     }
 }
