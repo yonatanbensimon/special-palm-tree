fcb9c1cff91f0871688b126c9c6dc2a81f5a4f09
Aurélia Chu Bouliane <aurelia.bouliane@gmail.com>
2026-02-01 12:59:43 -0500

    Completed the Game Over and Insanity Game Over pages abd their transitions

---
+++ Special Palm Tree/Assets/Yasmine_scripts/GameOverTransition.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverTransition : MonoBehaviour
+{
+    [Header("Delay before going to Main Menu")]
+    public float delaySeconds = 5f;
+
+    [Header("Blackout Fade")]
+    public Image fadeImage;
+    public float fadeDuation = 1f;
+
+    void Start()
+    {
+        StartCoroutine(WaitAndFade());
+    }
+    IEnumerator WaitAndFade()
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        if (fadeImage != null)
+        {
+            fadeImage.gameObject.SetActive(true);
+            Color c = fadeImage.color;
+            c.a = 0;
+            fadeImage.color = c;
+
+            float time = 0;
+            while(time < fadeDuation)
+            {
+                time += Time.deltaTime;
+                c.a = Mathf.Clamp01(time/fadeDuation);
+                fadeImage.color= c;
+                yield return null;
+            }
+        }
+        SceneManager.LoadScene("TitlePageScene");
+    }
+}
